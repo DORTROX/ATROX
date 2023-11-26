f156035304b3c276df43ea6b41c2ff1ca69db5a7
DORTROX プロ <levisaustin011@gmail.com>
2023-11-26 23:01:41 +0530

    Secure Next Auth Token Cookie

--- app/api/SwitchAccount/route.ts
+++ app/api/SwitchAccount/route.ts
@@ -4,6 +4,6 @@ import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest) {
   const { token } = await req.json();
-  setCookie("next-auth.session-token", token, {cookies});
+  setCookie("__Secure-next-auth.session-token", token, {cookies});
   return new Response(JSON.stringify("OK"));
 }

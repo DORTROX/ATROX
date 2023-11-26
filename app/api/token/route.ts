3c6f99ee27eb1e2a686d15cc1a7d331067d6893c
DORTROX プロ <levisaustin011@gmail.com>
2023-11-26 23:40:47 +0530

    Auth Fix

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,7 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
+const secret = process.env.NEXTAUTH_SECRET
 
 export async function GET(req: NextRequest) {
-  const token = await getToken({ req });
+  const token = await getToken({ req, secret });
   return new Response(JSON.stringify(token));
 }

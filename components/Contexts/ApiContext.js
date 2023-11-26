5c9a081762c756131eacb6d28de3bf50a0737e7d
DORTROX プロ <levisaustin011@gmail.com>
2023-11-26 18:02:19 +0530

    Cleaning Workspace

--- components/Contexts/ApiContext.js
+++
@@ -1,7 +0,0 @@
-export const ProtectRoute = ({ children }) => {
-    const { isAuthenticated, isLoading } = useAuth();
-    if (isLoading || (!isAuthenticated && window.location.pathname !== '/login')){
-      return <LoadingScreen />; 
-    }
-    return children;
-  }

b1102a6fd268f826ab3605341789154da7f0ffea
DORTROX プロ <levisaustin011@gmail.com>
2023-11-27 15:15:14 +0530

    Api Fix 5

--- app/api/SwitchAccount/route.ts
+++ app/api/SwitchAccount/route.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest) {
   const { token } = await req.json();
-  setCookie("next-auth.session-token", token, {cookies, domain: '.vercel.app', secure: true, httpOnly: true, sameSite: 'none'});
+  setCookie("__Secure-next-auth.session-token", token, {cookies});
   return new Response(JSON.stringify("OK"));
 }
 // next-auth.session-token __Secure-next-auth.session-token

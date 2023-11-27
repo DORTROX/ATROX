e0fe2ea842bfaf08e3007c9ed74e153d9fb0b80c
DORTROX プロ <levisaustin011@gmail.com>
2023-11-27 20:00:35 +0530

    Api Fix v6

--- app/api/SwitchAccount/route.ts
+++ app/api/SwitchAccount/route.ts
@@ -1,10 +1,18 @@
-import { NextRequest } from "next/server";
-import { setCookie } from "cookies-next";
-import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+// import { setCookie } from "cookies-next";
+// import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest) {
   const { token } = await req.json();
-  setCookie("__Secure-next-auth.session-token", token, {cookies});
-  return new Response(JSON.stringify("OK"));
+
+  // setCookie("next-auth.session-token", token, {cookies});
+  const response = NextResponse.json("OK",
+    { status: 200, statusText: "Set cookie successfully" }
+  );
+  response.cookies.set({
+    name: "__Secure-next-auth.session-token",
+    value: token
+  })
+  return response
 }
 // next-auth.session-token __Secure-next-auth.session-token
